4b04d6874ff8800caed8c4a88e6effb1079ad6ff
Michele Mancioppi <michelem@lumigo.io>
2022-06-14 10:45:21 +0200

    Add .env support

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,35 @@
 import { App } from 'aws-cdk-lib';
+import 'dotenv/config'
+
 // import { MyContainerStack } from './container-stack';
 import { MyLambdaStack } from './lambda-stack';
+
+var path = require('path');
+var dotenv = require('dotenv')
+
+/*
+ * Reads the following vars from `<repo_root>/.env` and sets them
+ * on the process as environment variables:
+ *
+ * ```
+ * ACCOUNT=538118019757
+ * REGION=eu-central-1
+ * ```
+ */
+dotenv.config({ path: path.join(path.dirname(__dirname), '.env') })
 
 const app = new App();
 
 new MyLambdaStack(app, 'lumigo-webinar-lambda', {
   env: {
-    account: '538118019757',
-    region: 'eu-central-1',
+    region: process.env.REGION,
+    account: process.env.ACCOUNT,
   },
 });
 // new MyContainerStack(app, 'lumigo-webinar-container', {
 //   env: {
-//     account: '538118019757',
-//     region: 'eu-central-1',
+//     region: process.env.REGION,
+//     account: process.env.ACCOUNT,
 //   },
 // });
 
